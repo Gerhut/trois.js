1e136a9935049f2156dee0fd80ab172c7108ef34
George Chung <Gerhut@GMail.com>
2015-12-13 14:31:02 +0800

    move material cache from Vue.prototype to Vue itself

--- src/plugins/materialCache.js
+++ src/plugins/materialCache.js
@@ -1,16 +1,16 @@
 export default {
   install (Vue) {
+    Vue._troisMaterialCache = {}
     Object.assign(Vue.prototype, {
-      _troisMaterialCache: {},
       _troisMaterialCacheAdd () {
-        this._troisMaterialCache[this._uid] = this
+        Vue._troisMaterialCache[this._uid] = this
       },
       _troisMaterialCacheRemove () {
-        delete this._troisMaterialCache[this._uid]
+        delete Vue._troisMaterialCache[this._uid]
       },
       _troisMaterialCacheUpdate () {
-        for (let uid in this._troisMaterialCache) {
-          this._troisMaterialCache[uid].$trois.needsUpdate = true
+        for (let uid in Vue._troisMaterialCache) {
+          Vue._troisMaterialCache[uid].$trois.needsUpdate = true
         }
       }
     })
